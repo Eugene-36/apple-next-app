19e3ebda20eda9bb2f4c6a3a595558d45a17d0d8
Eugene-36 <livenskiyjenya@gmail.com>
2025-10-21 17:21:52 +0300

    slider-fix

--- src/components/HeroSlider/HeroSlider.jsx
+++ src/components/HeroSlider/HeroSlider.jsx
@@ -1,21 +1,40 @@
 'use client';
 
+import { slides } from '@data/hero-slider';
+import Image from 'next/image';
 import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 const HeroSlider = () => {
 	return (
 		<Swiper
-			spaceBetween={50}
-			slidesPerView={3}
-			onSlideChange={() => console.log('slide change')}
-			onSwiper={(swiper) => console.log(swiper)}
+			spaceBetween={30}
+			slidesPerView={'auto'}
+			centeredSlides={true}
+			className="mySwiper"
+			autoplay={{ delay: 2500, disableOnInteraction: false }}
+			modules={[Autoplay, Pagination]}
+			pagination={{
+				clickable: true,
+			}}
+			speed={1000}
+			loop={true}
 		>
-			<SwiperSlide>Slide 1</SwiperSlide>
-			<SwiperSlide>Slide 2</SwiperSlide>
-			<SwiperSlide>Slide 3</SwiperSlide>
-			<SwiperSlide>Slide 4</SwiperSlide>
-			...
+			{slides.map(({ id, srcImg, alt }) => (
+				<SwiperSlide>
+					<Image
+						key={id}
+						src={srcImg}
+						width={1250}
+						height={668}
+						sizes="(max-width: 1200px) 100vw, 1250px"
+						priority
+						alt={alt}
+					/>
+				</SwiperSlide>
+			))}
 		</Swiper>
 	);
 };
---
+++ src/data/hero-slider.js
@@ -0,0 +1,52 @@
+export const slides = [
+	{
+		id: 1,
+		srcImg: '/img/hero-slider/01.jpg',
+		alt: 'Hero text alt',
+	},
+	{
+		id: 2,
+		srcImg: '/img/hero-slider/02.jpg',
+		alt: 'Hero text alt',
+	},
+	{
+		id: 3,
+		srcImg: '/img/hero-slider/03.jpg',
+		alt: 'Hero text alt',
+	},
+	{
+		id: 4,
+		srcImg: '/img/hero-slider/04.jpg',
+		alt: 'Hero text alt',
+	},
+	{
+		id: 5,
+		srcImg: '/img/hero-slider/05.jpg',
+		alt: 'Hero text alt',
+	},
+	{
+		id: 6,
+		srcImg: '/img/hero-slider/06.jpg',
+		alt: 'Hero text alt',
+	},
+	{
+		id: 7,
+		srcImg: '/img/hero-slider/07.jpg',
+		alt: 'Hero text alt',
+	},
+	{
+		id: 8,
+		srcImg: '/img/hero-slider/08.jpg',
+		alt: 'Hero text alt',
+	},
+	{
+		id: 9,
+		srcImg: '/img/hero-slider/09.jpg',
+		alt: 'Hero text alt',
+	},
+	{
+		id: 10,
+		srcImg: '/img/hero-slider/10.jpg',
+		alt: 'Hero text alt',
+	},
+];
